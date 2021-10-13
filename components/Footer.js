70a9593de66b7fae141a3fc5d1895f56a28b2e9c
Aiden <aidenjiahao@gmail.com>
2021-10-13 19:45:56 -0400

    finish the page build of day 2

---
+++ components/Footer.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-400 ">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Arirbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>Its a pretty awesome clone</p>
+        <p>Referrals accepted</p>
+        <p>Low cost</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>React Project</p>
+        <p>Presents</p>
+        <p> More customers</p>
+        <p>Hundards of rates</p>
+        <p>Join Now</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Centre</p>
+        <p>Trust && Safety</p>
+        <p>Youtube Link</p>
+        <p>Holidays Schedule</p>
+        <p>Make Better Life</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
