353c4682b5336968c3b26de6a0dba049165985fc
Aiden <aidenjiahao@gmail.com>
2021-10-15 14:23:16 -0400

    all done, before deploy to vercel

---
+++ components/Map.js
@@ -0,0 +1,65 @@
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { useState } from "react";
+import getCenter from "geolib/es/getCenter";
+
+function Map({ searchResults }) {
+  const [selectedLocation, setSelectedLocation] = useState({});
+
+  // now we need to transform searchResults (bunch of data) into the one we need ( latitude, longitude) only.
+  const coordinates = searchResults.map((result) => ({
+    longitude: result.long,
+    latitude: result.lat,
+  }));
+  // in order to put all the coordinates in one map , we need a center point. The latitude and longitude of the center of locations coordinates.
+  const center = getCenter(coordinates);
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: center.latitude,
+    longitude: center.longitude,
+    zoom: 11,
+  });
+
+  return (
+    <ReactMapGL
+      mapStyle="mapbox://styles/aidenkagou/ckusl9jrc9za917ph7lwdbr7l"
+      mapboxApiAccessToken={process.env.mapbox_key}
+      {...viewport}
+      onViewportChange={(viewport) => setViewport(viewport)}
+    >
+      {searchResults.map((result) => (
+        <div key={result.long}>
+          <Marker
+            longitude={result.long}
+            latitude={result.lat}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <p
+              onClick={() => setSelectedLocation(result)}
+              className="cursor-pointer text-xl animate-bounce text-red-300 "
+            >
+              Here!
+            </p>
+          </Marker>
+
+          {/*this is the popup to show if we clikc on the marker */}
+          {selectedLocation.long === result.long ? (
+            <Popup
+              onClose={() => setSelectedLocation({})}
+              closeOnClick={true}
+              latitude={result.lat}
+              longitude={result.long}
+            >
+              {result.title}
+            </Popup>
+          ) : (
+            false
+          )}
+        </div>
+      ))}
+    </ReactMapGL>
+  );
+}
+
+export default Map;
--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiYWlkZW5rYWdvdSIsImEiOiJja3VzbDIxNXI1ZzM3MnBtbmJuaDk3amk4In0.wmax7hoqlpBW0659HfbbRg",
+  },
 };
